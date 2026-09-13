db090461525f436a29cffb34ca2af1a240753b89
brenoxavier48 <brenoxavier8.bx@gmail.com>
2026-09-13 20:55:54 -0300

    test: add mocks for repository

---
+++ application/mocks/use_cases.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"github.com/brenoxavier48/POC---Hexagonal-Design/application/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockProductPersistence struct {
+	mock.Mock
+}
+
+func (m *MockProductPersistence) Get(id string) (*domain.IProduct, error) {
+	args := m.Called(id)
+
+	product, _ := args.Get(0).(*domain.IProduct)
+
+	return product, args.Error(1)
+}
+
+func (m *MockProductPersistence) Save(product *domain.IProduct) error {
+	args := m.Called(product)
+	return args.Error(0)
+}
